67acb6a4a08110ce65c6429fa185a1534c8c0c14
mahmoudproto <mahmoud_proto@hotmail.com>
2025-03-20 10:36:00 +0200

    solved uploading to github pages default page that has an diffrent intial pathname thank '/'

--- src/App.js
+++ src/App.js
@@ -6,32 +6,36 @@ import ButtonPage from "./Pages/ButtonPage";
 import ModalPage from "./Pages/ModalPage";
 import TablePage from "./Pages/TablePage";
 import CounterPage from "./Pages/CounterPage";
-
-
+import { useEffect, useRef } from "react";
 
 function APP() {
+  //added to solve uploading to github pages default page that has an diffrent intial pathname thank '/'
+  const homePagePath = useRef("/");
+  useEffect(() => {
+    homePagePath.current = window.location.pathname;
+  }, []);
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
-      <Sidebar/>
+      <Sidebar />
       <div className="col-span-5">
-      <Route path={'/'}>
-        <DropDownPage/>
-      </Route>
-      <Route path={'/accordion'}>
-        <AccordionPage/>
-      </Route>
-      <Route path={'/buttons'}>
-        <ButtonPage/>
-      </Route>
-      <Route path={'/table'}>
-        <TablePage/>
-      </Route>
-      <Route path={'/modal'}>
-        <ModalPage/>
-      </Route>
-      <Route path={'/count'}>
-        <CounterPage />
-      </Route>
+        <Route path={homePagePath.current}>
+          <DropDownPage />
+        </Route>
+        <Route path={"/accordion"}>
+          <AccordionPage />
+        </Route>
+        <Route path={"/buttons"}>
+          <ButtonPage />
+        </Route>
+        <Route path={"/table"}>
+          <TablePage />
+        </Route>
+        <Route path={"/modal"}>
+          <ModalPage />
+        </Route>
+        <Route path={"/count"}>
+          <CounterPage />
+        </Route>
       </div>
     </div>
   );
